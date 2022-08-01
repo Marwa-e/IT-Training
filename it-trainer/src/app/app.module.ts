a132f98ceaec15c37d391e7992153ca4efc4c5e3
Marwa-e <marwa.ellijimi@gmail.com>
2022-08-01 02:03:35 +0200

    ajout composant inscription et login

--- it-trainer/src/app/app.module.ts
+++ it-trainer/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientComponent } from './composants/client/client.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InternauteComponent } from './composants/internaute/internaute.component';
 import { MenuComponent } from './composants/menu/menu.component';
 import { CarousselComponent } from './composants/caroussel/caroussel.component';
 import { RechercherComponent } from './composants/rechercher/rechercher.component';
 import { FormationComponent } from './composants/formation/formation.component';
+import { LoginComponent } from './composants/login/login.component';
+import { InscriptionComponent } from './composants/inscription/inscription.component';
+import { HomeComponent } from './composants/home/home.component';
+
 
 
 
@@ -19,16 +23,22 @@ import { FormationComponent } from './composants/formation/formation.component';
     AppComponent,
     ClientComponent,
     InternauteComponent,
+    HomeComponent,
     MenuComponent,
     FormationComponent,
     CarousselComponent,
-    RechercherComponent
+    RechercherComponent,
+    LoginComponent,
+    InscriptionComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
