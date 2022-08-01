436681abd7e6e1c24ab78050649a3f5deadadfb5
Marwa-e <marwa.ellijimi@gmail.com>
2022-08-01 17:08:53 +0200

    avancement sur le bouton inscription

--- it-trainer/src/app/interfaces/internaute.ts
+++ it-trainer/src/app/interfaces/internaute.ts
@@ -1,8 +1,9 @@
 
 import { Adresse } from "./adresse";
+import { Client } from "./client";
 import { Telephone } from "./telephone";
 
-export interface Internaute {
+export interface Internaute extends Client {
 
     id?: number
     nom?: string
