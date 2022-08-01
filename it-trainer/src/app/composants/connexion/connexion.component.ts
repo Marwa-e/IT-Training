00e8e8055d684808d0bdf81b92bab0e9a9d753e9
CaptainMurdock07 <guillaume.resin06@gmail.com>
2022-08-01 10:07:03 +0200

    modif connexion guillaume 4

--- it-trainer/src/app/composants/connexion/connexion.component.ts
+++ it-trainer/src/app/composants/connexion/connexion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Internaute } from 'src/app/interfaces/internaute';
-import { UserService } from 'src/app/services/user.service';
+import { InternauteService } from 'src/app/services/internaute.service';
 
 @Component({
   selector: 'app-connexion',
@@ -12,7 +12,7 @@ export class ConnexionComponent implements OnInit {
 
   utilisateur: Internaute = {};
   isConnected = false;
-  constructor(private router: Router, private us: UserService) { }
+  constructor(private router: Router, private us: InternauteService) { }
 
   ngOnInit(): void {
   }
