9cbaea79b70b8389de68439c6fe20cbf66cf6997
Marwa-e <marwa.ellijimi@gmail.com>
2022-08-02 09:55:52 +0200

    ajout menu Narwa

---
+++ it-trainer/src/app/composants/menu/menu.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categorie } from 'src/app/interfaces/categorie';
+import { CategorieService } from 'src/app/services/categorie.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  categoriesInitiales:Categorie[]=[]
+  constructor(private route: ActivatedRoute,private cat:CategorieService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.cat.findByMere(1).subscribe(resul => {
+      this.categoriesInitiales=resul;
+    })
+  }
+
+  openPopup(){
+
+  }
+
+  closePopup(){
+    
+  }
+}
